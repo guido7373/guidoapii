d768f7ae0526f0056b0ac0a54d23db182f4ff933
guido <guidofacio91@gmail.com>
2023-09-29 12:29:32 -0300

    15

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const cors = require("cors");
 
 const displayRoutes = require("express-routemap");
 
-const connectDB = require("db");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  try {
+    PORT = 5000
+    MONGO_URI = "mongodb+srv://guido:guido@guido0.ybbt6ma.mongodb.net/guido?retryWrites=true&w=majority"
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 connectDB();
 
