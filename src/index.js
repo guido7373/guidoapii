bd64917ffc9cbd1dfe4d7b7e75b75299e390fa31
guido <guidofacio91@gmail.com>
2023-09-28 22:44:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const displayRoutes = require("express-routemap");
+const cors = require("cors");
+const { MongoClient } = require("mongodb"); // Importa MongoClient de mongodb
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const BASE_PREFIX = process.env.BASE_PREFIX || "api";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// URL de conexión a MongoDB Atlass
+const uri = "mongodb+srv://guido:guido@guido0.ybbt6ma.mongodb.net/?retryWrites=true&w=majority";
+
+app.use("/static", express.static(`${__dirname}/public`));
+
+app.get(`/`, (req, res) => {
+  return res.json({ message: `API DEPLOY SUCCESS` });
+});
+
+// Mueve la conexión a la base de datos fuera de la ruta
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get(`/${BASE_PREFIX}/data`, async (req, res) => {
+  try {
+    // Conecta al cliente de MongoDB una vez al inicio de la aplicación
+    if (!client.isConnected()) {
+      await client.connect();
+    }
+
+    // Accede a la base de datos y la colección
+    const database = client.db("guido");
+    const collection = database.collection("guidocolec");
+
+    // Consulta los registros de la colección
+    const datos = await collection.find({}, { _id: 0, nombre: 1, apellido: 1 }).toArray();
+
+    // Envía los datos como respuesta en formato JSON
+    res.json(datos);
+  } catch (error) {
+    console.error('Error al obtener datos:', error);
+    res.status(500).json({ error: 'Error al obtener datos' });
+  }
+});
+
+app.listen(PORT, () => {
+  displayRoutes(app);
+  console.log(`API RUNNING ON PORT ${PORT}`);
+});
