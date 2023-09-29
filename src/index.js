d0be5407135ea964156be17c655e46c358547d02
guido <guidofacio91@gmail.com>
2023-09-28 23:33:11 -0300

    6commit

--- src/index.js
+++ src/index.js
@@ -22,40 +22,40 @@ app.get(`/`, (req, res) => {
   return res.json({ message: `API DEPLOY SUCCESS` });
 });
 
-// Mueve la conexión a la base de datos fuera de la ruta
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
-
-// Llama a la función para conectar a la base de datos al iniciar la aplicación
-connectToDatabase();
-
-app.get(`/${BASE_PREFIX}/data`, async (req, res) => {
-  try {
-    // Accede a la base de datos y la colección
-    const database = client.db("guido");
-    const collection = database.collection("guidocolec");
-
-    // Consulta los registros de la colección
-    const datos = await collection.find({}, { _id: 0, nombre: 1, apellido: 1 }).toArray();
-
-    // Envía los datos como respuesta en formato JSON
-    res.json(datos);
-  } catch (error) {
-    console.error('Error al obtener datos:', error);
-    res.status(500).json({ error: 'Error al obtener datos' });
-  }
-});
+//// Mueve la conexión a la base de datos fuera de la ruta
+//const client = new MongoClient(uri, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true,
+//});
+//
+//async function connectToDatabase() {
+//  try {
+//    await client.connect();
+//    console.log('Connected to MongoDB');
+//  } catch (error) {
+//    console.error('Error connecting to MongoDB:', error);
+//  }
+//}
+//
+//// Llama a la función para conectar a la base de datos al iniciar la aplicación
+//connectToDatabase();
+//
+//app.get(`/${BASE_PREFIX}/data`, async (req, res) => {
+//  try {
+//    // Accede a la base de datos y la colección
+//    const database = client.db("guido");
+//    const collection = database.collection("guidocolec");
+//
+//    // Consulta los registros de la colección
+//    const datos = await collection.find({}, { _id: 0, nombre: 1, apellido: 1 }).toArray();
+//
+//    // Envía los datos como respuesta en formato JSON
+//    res.json(datos);
+//  } catch (error) {
+//    console.error('Error al obtener datos:', error);
+//    res.status(500).json({ error: 'Error al obtener datos' });
+//  }
+//});
 
 app.post("/books", (req, res) => {
   const data = readData();
