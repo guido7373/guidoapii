93adab755c9c9981d55494121888bc127b4d8e13
guido <guidofacio91@gmail.com>
2023-09-29 12:24:55 -0300

    16

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
+    PORT = 5000
+    MONGO_URI = "mongodb+srv://guido:guido@guido0.ybbt6ma.mongodb.net/guido?retryWrites=true&w=majority"
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
